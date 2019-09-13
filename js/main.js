713a0fe68b1a8d0fa6a200b1c262220b7deb5d83
unknown <raszewska.public@gmail.com>
2019-09-13 12:48:22 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $(".rwdMenu").click(function(){
+        $(".topMenu").toggleClass("block").slideToggle();    
+    });
+    $(".topMenu li").click(function(){
+        if ($(".rwdMenu").is(":visible")) {
+            $(".topMenu").slideToggle();    
+        }
+    });
+});
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+     
+    
